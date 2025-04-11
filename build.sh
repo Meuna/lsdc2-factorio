7ce53ec0362b2b3fd126f8dec7d2fef86d213ab2
Thomas Bany <mun.bany@gmail.com>
2025-04-11 21:04:04 +0200

    Build script can now be run from any wd

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-podman build . -t docker.io/meuna/lsdc2:factorio \
+
+script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+podman build $script_dir -t docker.io/meuna/lsdc2:factorio \
 && podman push docker.io/meuna/lsdc2:factorio
